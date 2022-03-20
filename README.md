8f695db4b1b20d9b64e95446c9e28bdd5ee8899c
shppalushi <shppalushi@gmail.com>
2022-03-20 13:30:23 +0100

    Wrapped up ocr

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# document-ocr
+# Node js ocr document reader
+It can accept images to be uploaded
+Performs the operation of converting image to text using the trained data from `eng.traineddata`.
+Returns the text in html format.
+## How it works  
+After you upload the file, it reads it from request body and saves it under resources. On that saved file we perform the OCR to read over the image pixels.
+The result gets returned as http response.  
+## How to put it into work  
+1. Clone the repo.
+2. `npm i` - for installing the packages
+3. npm start for starting the application
+4. At `http://localhost:8080/upload` make a http post request to  upload the file as formdata with key file
+5. Wait for response.
+6. You can train the data even more on your needs and for that you have to refer `tesseract.js` documentation.
---
+++ src/controller/file.controller.js
@@ -0,0 +1,83 @@
+const uploadFile = require("../middleware/upload");
+const fs = require("fs");
+const baseUrl = "http://localhost:8080/files/";
+var Tesseract = require('tesseract.js');
+
+const upload = async (req, res) => {
+  try {
+    await uploadFile(req, res);
+
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Please upload a file!" });
+    }
+
+    var image = fs.readFileSync(
+      __basedir + '/resources/static/assets/uploads/' + req.file.originalname,
+      {
+        encoding: null
+      }
+    );
+    Tesseract.recognize(image)
+      .progress(function (p) {
+        console.log('progress', p);
+      })
+      .then(function (result) {
+        res.send(result.html);
+      });
+
+  } catch (err) {
+    console.log(err);
+
+    if (err.code == "LIMIT_FILE_SIZE") {
+      return res.status(500).send({
+        message: "File size cannot be larger than 2MB!",
+      });
+    }
+
+    res.status(500).send({
+      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+    });
+  }
+};
+
+const getListFiles = (req, res) => {
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      res.status(500).send({
+        message: "Unable to scan files!",
+      });
+    }
+
+    let fileInfos = [];
+
+    files.forEach((file) => {
+      fileInfos.push({
+        name: file,
+        url: baseUrl + file,
+      });
+    });
+
+    res.status(200).send(fileInfos);
+  });
+};
+
+const download = (req, res) => {
+  const fileName = req.params.name;
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+  res.download(directoryPath + fileName, fileName, (err) => {
+    if (err) {
+      res.status(500).send({
+        message: "Could not download the file. " + err,
+      });
+    }
+  });
+};
+
+module.exports = {
+  upload,
+  getListFiles,
+  download,
+};
